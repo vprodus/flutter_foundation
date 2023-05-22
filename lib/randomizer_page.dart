81620852db5e7af290362c113365a8cf0c6779e8
Vadim Vprodus <vproduspro@gmail.com>
2023-05-23 00:00:32 +0300

    Pass the parameters to the widget and generate the random number between them by pressing the button

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class RandomizerPage extends StatefulWidget {
@@ -9,12 +11,30 @@ class RandomizerPage extends StatefulWidget {
 }
 
 class _RandomizerPageState extends State<RandomizerPage> {
+  int? _generatedNumber;
+  final randomGenerator = Random();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Randomizer'),
       ),
+      body: Center(
+        child: Text(
+          _generatedNumber?.toString() ?? 'Generate a number',
+          style: TextStyle(fontSize: 42),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Generate'),
+        onPressed: () {
+          setState(() {
+            _generatedNumber = widget.min +
+                randomGenerator.nextInt(widget.max - widget.min + 1);
+          });
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
