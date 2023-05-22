fd606fafafc43129de161c8dfbb4a1edcb496464
Vadim Vprodus <vproduspro@gmail.com>
2023-05-22 22:51:20 +0300

    Creating the Input Forrm UI

--- lib/range_selector_page.dart
+++ lib/range_selector_page.dart
@@ -15,7 +15,19 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
         title: Text('Select Range'),
       ),
       body: Form(
-        child: Container(),
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RangeForm('Maximum'),
+                const SizedBox(height: 12),
+                RangeForm('Minimum'),
+              ],
+            ),
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_forward),
@@ -25,5 +37,18 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
         },
       ),
     ); // This trailing comma makes auto-formatting nicer for build methods.
+  }
+
+  TextFormField RangeForm(label) {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: label,
+      ),
+      keyboardType: TextInputType.numberWithOptions(
+        decimal: false,
+        signed: true,
+      ),
+    );
   }
 }
